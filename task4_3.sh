bb1749a7e89e7104898913c69ff28d703b9c21ac
Alexander Serebrov <serebrov.alexandr@gmail.com>
2018-03-29 22:47:56 +0300

    Progress

--- task4_3.sh
+++ task4_3.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-$target_folder="/home/administrator/web/logs/"
-cp -rf "$target_folder" /tmp/backups/
+# Get current date
+DATE=`date +%Y-%m-%d`
 
-arch_name=$(echo $target_folder | tr "/" "-" | sed 's/^-\(.*\)-$/\1/')
+target_folder='/home/administrator/web/logs/'
+final_dir="${target_folder}${1##*/}"
+echo "$final_dir"
 
+# Copy target folder to /tmp/backups
+cp -r "$target_folder" /tmp/backups/
+
+HITSDIR=`find "/tmp/backups/" -type d | sed -e 's|'$target_folder'\(.*\)|\1|g'`
+echo $HITSDIR
+# Make arch_name as /some/path/ => some-path
+arch_name=$(echo $target_folder | tr "/" "-" | sed 's/\(^-\)\|\(-$\)//')
+# more correct echo $target_folder | tr "/" "-" | sed 's/^-\(.*\)$-/\1/'
+echo $arch_name
+tar -cvzf /tmp/backups/"$arch_name_$DATE".tar.gz "$HITSDIR"

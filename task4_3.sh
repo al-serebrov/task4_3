3b90b2a4721e0c9d1709ae45555f1cbdb9e7f96c
Alexander Serebrov <serebrov.alexandr@gmail.com>
2018-03-30 12:28:03 +0300

    Correct script to work according to requirements

--- task4_3.sh
+++ task4_3.sh
@@ -1,19 +1,66 @@
 #!/bin/bash
 
-# Get current date
-DATE=`date +%Y-%m-%d`
+function check_backups_qty () {
+  # Find all backups of target_folder in /tmp/backups and check
+  # if there are too many of them
+  # Returns:
+  #   true - there are more backups than allowed
+  #   false - there are less backups than allowed
+  backed_up_qty=$(find /tmp/backups/ -name "*$arch_name*" | wc -l);
+  echo $backed_up_qty
+  [[ "$backed_up_qty" -gt "$max_backup_qty" ]] && return
+}
 
-target_folder='/home/administrator/web/logs/'
-final_dir="${target_folder}${1##*/}"
-echo "$final_dir"
+# Check if there are 2 command line arguments passed
+if [ "$#" -ne 2 ]; then
+    echo "Illegal number of parameters" >&2
+    exit 1
+fi
+
+# Assign variables
+target_folder="$1"  #'/home/administrator/web/logs'
+max_backup_qty="$2"
+
+# Check if target folder exists
+if [ ! -d "$target_folder" ]; then
+  echo "Taget directory doesn't exist" >&2
+  exit 1
+fi
+
+# Check if number of backups is valid integer, e.g. 0, 1, 2 etc are valid
+# -1, lol, *22 - invalid
+if [[ ! $max_backup_qty =~ ^[0-9]+$ ]]; then
+  echo "Illegal number of allowed backups" >&2
+  exit 1
+fi
+
+# backup_path="{target_folder}${1##*/}"
+backup_path="/tmp/backups"
+
+# Create folder for backups if it doesn't exist
+if [ ! -d "$backup_path" ]; then
+  mkdir "/tmp/backups"
+fi
+
+# Get current date in epoch format (seconds since 01-01-1970)
+DATE=`date +%s`
+
+# Make arch_name as /some/path/ => some-path
+# To replace slashes to dashes: tr "/" "-"
+# To trim the leading dash (if any): sed 's/^-//'
+# To trim the trailing dash (if any): sed 's/-$//'
+arch_name=$(echo $target_folder | tr "/" "-" | sed 's/^-//' | sed 's/-$//')
 
 # Copy target folder to /tmp/backups
-cp -r "$target_folder" /tmp/backups/
+cp -r "$target_folder" "$backup_path"/"$arch_name"
 
-HITSDIR=`find "/tmp/backups/" -type d | sed -e 's|'$target_folder'\(.*\)|\1|g'`
-echo $HITSDIR
-# Make arch_name as /some/path/ => some-path
-arch_name=$(echo $target_folder | tr "/" "-" | sed 's/\(^-\)\|\(-$\)//')
-# more correct echo $target_folder | tr "/" "-" | sed 's/^-\(.*\)$-/\1/'
-echo $arch_name
-tar -cvzf /tmp/backups/"$arch_name_$DATE".tar.gz "$HITSDIR"
+# Make a .tar.gz archive and remove temporary folder
+tar -cvzf /tmp/backups/"$arch_name"_"$DATE".tar.gz "$backup_path"/"$arch_name"
+rm -rf "$backup_path"/"$arch_name"
+
+# Check if there are more backups than allowed
+while check_backups_qty; do
+  oldest_filename=$(find /tmp/backups -type f -printf '%p\n' | sort | grep "$arch_name" | head -n 1)
+  rm "$oldest_filename"
+done
+
